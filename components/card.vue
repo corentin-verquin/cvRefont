77b94a783c74fc1e9f53933c7bdab5895abe051b
corentin verquin <corentin.verquin@gmail.com>
2020-12-30 17:52:03 +0100

    feat(card): create card component

---
+++ components/card.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  .card
+    .card__content
+      slot
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+
+@Component
+export default class Card extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/scss/config';
+@import '../assets/scss/components/card.scss';
+</style>
