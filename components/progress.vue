3cfb969a4bda06e272d8d48aee89200818d775bd
corentin verquin <corentin.verquin@gmail.com>
2020-12-30 17:53:25 +0100

    feat(progress): create progress component

---
+++ components/progress.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+  .progress__container(@mouseover="hover()",@mouseleave="leave()")
+    .progress__wrapper
+      span.progress(:style="getWidth",:data-value="value", :class="{ hover: isHover }")
+</template>
+>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'nuxt-property-decorator'
+
+@Component
+export default class Progress extends Vue {
+  @Prop({ type: Number, required: true }) readonly value!: number
+
+  isHover: boolean = false
+
+  get getWidth() {
+    return `width: ${this.value}%`
+  }
+
+  hover() {
+    this.isHover = true
+  }
+
+  leave() {
+    this.isHover = false
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../assets/scss/config';
+@import '../assets/scss/components/progress';
+</style>
