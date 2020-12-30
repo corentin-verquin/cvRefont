c80b8d4abb2b7875f52410b22fa41b0aa53b1335
corentin verquin <corentin.verquin@gmail.com>
2020-12-30 17:52:52 +0100

    feat(mardown): create mardown component

---
+++ components/markdown.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="markown-container" v-html="$md.render(content)"></div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'nuxt-property-decorator'
+
+@Component
+export default class Markdown extends Vue {
+  @Prop({ type: String, required: true }) readonly content!: string
+}
+</script>
+
+<style lang="scss">
+@import '../assets/scss/components/markdown';
+</style>
